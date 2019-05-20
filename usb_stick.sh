d526113b0a29e6c406a293d740c2e9594967058b
llenotre <llenotre@student.42.fr>
2019-05-20 13:27:36 +0200

    Added usb_stick.sh

---
+++ usb_stick.sh
@@ -0,0 +1,11 @@
+read -p "Type device to use as a USB stick (warning: this will erase all data on it): /dev/" device_name
+read -p "Type device label (warning: this will erase all data on it): /media/" label
+
+device=/dev/$device_name
+media=/media/$label
+new_label=crumbleos
+
+if sudo mkfs.vfat -F 32 -n $new_label -I $device;
+then
+	sudo grub-install --root-directory=$media --no-floppy --recheck --force $device
+fi
