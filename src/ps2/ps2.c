5f79931199caa76a6ca89354ca2ca950e97fd837
llenotre <llenotre@student.42.fr>
2019-05-23 04:29:55 +0200

    Keyboard init WIP

--- src/ps2/ps2.c
+++ src/ps2/ps2.c
@@ -1,5 +1,39 @@
 #include "ps2.h"
 
+__attribute__((hot))
+__attribute__((const))
+static inline bool can_read(void)
+{
+	return inb(PS2_STATUS) & 0b1;
+}
+
+__attribute__((hot))
+__attribute__((const))
+static inline void wait_read(void)
+{
+	while(!can_read())
+	{
+		// TODO Sleep?
+	}
+}
+
+__attribute__((hot))
+__attribute__((const))
+static inline bool can_write(void)
+{
+	return !(inb(PS2_STATUS) & 0b10);
+}
+
+__attribute__((hot))
+__attribute__((const))
+static inline void wait_write(void)
+{
+	while(!can_write())
+	{
+		// TODO Sleep?
+	}
+}
+
 // TODO Timeout
 __attribute__((hot))
 static uint8_t ps2_command(const uint8_t command,
@@ -10,13 +44,10 @@ static uint8_t ps2_command(const uint8_t command,
 
 	while(attempts++ < PS2_MAX_ATTEMPTS)
 	{
+		wait_write();
 		outb(PS2_COMMAND, command);
 
-		while(!(inb(PS2_STATUS) & 0b1))
-		{
-			// TODO Sleep?
-		}
-
+		wait_read();
 		if((response = inb(PS2_DATA)) == expected_response)
 			break;
 	}
@@ -28,23 +59,23 @@ __attribute__((hot))
 __attribute__((const))
 static inline bool test_controller(void)
 {
-	return (ps2_command(0xaa, CONTROLLER_TEST_PASS) == CONTROLLER_TEST_PASS
-		&& inb(PS2_DATA) == 0x55);
+	return (ps2_command(0xaa, CONTROLLER_TEST_PASS) == CONTROLLER_TEST_PASS);
 }
 
 __attribute__((hot))
 __attribute__((const))
 static inline bool test_device(void)
 {
-	return (ps2_command(0xab, KEYBOARD_TEST_PASS) == KEYBOARD_TEST_PASS
-		&& inb(PS2_DATA) == 0x00);
+	return (ps2_command(0xab, KEYBOARD_TEST_PASS) == KEYBOARD_TEST_PASS);
 }
 
 __attribute__((hot))
 __attribute__((const))
 void ps2_disable_devices(void)
 {
+	wait_write();
 	outb(PS2_COMMAND, 0xad);
+	wait_write();
 	outb(PS2_COMMAND, 0xa7);
 }
 
@@ -52,8 +83,25 @@ __attribute__((hot))
 __attribute__((const))
 bool ps2_enable_keyboard(void)
 {
+	wait_write();
 	outb(PS2_COMMAND, 0xae);
-	// TODO?
+
+	// TODO LEDs
+
+	if(ps2_command(0xf0, KEYBOARD_ACK) != KEYBOARD_ACK)
+		return false;
+
+	wait_write();
+	outb(PS2_DATA, 2);
+
+	if(ps2_command(0xf3, KEYBOARD_ACK) != KEYBOARD_ACK)
+		return false;
+
+	wait_write();
+	outb(PS2_DATA, 0);
+
+	if(ps2_command(0xf4, KEYBOARD_ACK) != KEYBOARD_ACK)
+		return false;
 
 	return true;
 }
@@ -62,7 +110,10 @@ __attribute__((hot))
 __attribute__((const))
 static inline uint8_t get_config_byte(void)
 {
+	wait_write();
 	outb(PS2_COMMAND, 0x20); // TODO Use ps2_command without expected response?
+
+	wait_read();
 	return inb(PS2_DATA);
 }
 
@@ -70,7 +121,9 @@ __attribute__((hot))
 __attribute__((const))
 static inline void set_config_byte(const uint8_t config_byte)
 {
+	wait_write();
 	outb(PS2_COMMAND, 0x60);
+	wait_write();
 	outb(PS2_DATA, config_byte); // TODO Check if can write before
 }
 
@@ -81,10 +134,9 @@ static void in_ps2_init(void)
 	ps2_disable_devices();
 	inb(PS2_DATA);
 
-	// TODO Correct?
 	set_config_byte(get_config_byte() & 0b10111100);
-	printf("PS/2, Dual Channel: %s\n",
-		(get_config_byte() & 0b10000 ? "no" : "yes"));
+	printf("PS/2 Dual Channel: %s\n",
+		((get_config_byte() & 0b100000) ? "no" : "yes"));
 
 	if(!test_controller())
 	{
