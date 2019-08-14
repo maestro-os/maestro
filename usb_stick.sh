45338071a7524f906f2f1c4ca57784fa1975b643
llenotre <llenotre@student.42.fr>
2019-08-14 17:12:36 +0200

    usb_stick.sh WIP

--- usb_stick.sh
+++ usb_stick.sh
@@ -1,16 +1,16 @@
+#!/bin/bash
+
+echo "Please unmount every partition on the device before proceeding."
 read -p "Type device to use as a USB stick (warning: this will erase all data on it): /dev/" device_name
-read -p "Type partition to format (warning: this will erase all data on it): /dev/" partition_name
 read -p "Type device label (warning: this will erase all data on it): /media/" label
 
 device=/dev/$device_name
-partition=/dev/$partition_name
 media=/media/$label
-new_label=crumbleos
+new_label=CRUMBLEOS
 
-umount $media
-if sudo mkfs.ext2 -F 32 -n $new_label -I $partition;
-then
-	sudo grub-install --root-directory=$media --target=i386-pc --no-floppy --recheck --force $device
-fi
-
-# TODO Copy kernel on device
+sudo mkfs.vfat -F 32 -n $new_label -I $device &&
+	sudo mkdir $media &&
+	sudo mount $device $media &&
+	sudo grub-install --root-directory=$media --target=i386-pc --no-floppy --recheck --force $device &&
+	sudo cp crumbleos $media/boot/ &&
+	sudo cp grub.cfg $media/boot/grub
