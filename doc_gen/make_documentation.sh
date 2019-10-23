55d8b509831b050b884f38bd7ea54ad4c0d004de
llenotre <llenotre@student.42.fr>
2019-10-23 22:55:40 +0200

    Added clean.sh

--- doc_gen/make_documentation.sh
+++ doc_gen/make_documentation.sh
@@ -2,15 +2,22 @@
 
 set -C noclobber
 
+IFS=''
 proto=`sh get_prototypes.sh`
-names=`echo $proto | awk 'print $4'`
+names=`echo $proto | awk '{print $4}'`
 
-IFS=''
-for $f in $proto;
+echo $proto | while read f;
 do
-	unset IFS
-	p=TODO
+{
 	n=`echo $f | awk '{print $1}'`
-	echo "```$p```TODO" > ${n}.md
-	IFS='' # TODO Remove?
+	if ! grep $n ignored >/dev/null;
+	then
+		p='TODO' # TODO Must be the entire prototype (currently, only the first line is in the variable)
+		echo "\`\`\`
+$p
+\`\`\`
+
+TODO" > ${n}.md 2>/dev/null
+	fi
+} 2>/dev/null
 done
