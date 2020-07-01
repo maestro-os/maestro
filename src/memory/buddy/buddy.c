bc096a355552964429c1e1ed7bedd41fbaef9af3
llenotre <llenotre@student.42.fr>
2020-07-02 00:41:36 +0200

    Buddy allocator WIP

--- src/memory/buddy/buddy.c
+++ src/memory/buddy/buddy.c
@@ -36,16 +36,21 @@
  */
 static block_state_t *blocks_states;
 
- /*
-  * The list of linked lists containing free blocks, sorted according to blocks'
-  * order.
-  */
-static block_state_t *free_list[BUDDY_MAX_ORDER + 1];
+/*
+ * The total number of allocatable pages.
+ */
+static size_t pages_count;
 
 /*
  * Pointer to the beginning of buddy memory.
  */
 static void *buddy_begin;
+
+/*
+ * The list of linked lists containing free blocks, sorted according to blocks'
+ * order.
+ */
+static block_state_t *free_list[BUDDY_MAX_ORDER + 1];
 
 /*
  * The spinlock used for buddy allocator operations.
@@ -81,7 +86,7 @@ block_order_t buddy_get_order(const size_t pages)
 ATTR_COLD
 void buddy_init(void)
 {
-	size_t pages_count, allocatable_memory;
+	size_t allocatable_memory;
 	size_t i, order;
 
 	blocks_states = mem_info.heap_begin;
@@ -109,20 +114,114 @@ void buddy_init(void)
 }
 
 /*
+ * Links the given element to the given free list.
+ */
+ATTR_HOT
+static void free_list_push(block_state_t **list, block_state_t *b)
+{
+	debug_assert(list && b, "Invalid arguments");
+	if(*list)
+	{
+		*b = **list;
+		*list = b;
+	}
+	else
+	{
+		*b = BLOCK_ID(b);
+		*list = b;
+	}
+}
+
+/*
+ * Unlinks the first element of the given free list.
+ */
+ATTR_HOT
+static void free_list_pop(block_state_t **list)
+{
+	block_state_t *b;
+
+	debug_assert(list && *list, "Invalid argument");
+	b = *list;
+	*list = &blocks_states[*b];
+	*b = BLOCK_STATE_USED;
+	if(**list == BLOCK_STATE_USED)
+		*list = NULL;
+}
+
+/*
+ * Splits the given block from order `from` to order `to`.
+ */
+static void free_list_split(const block_order_t from, const block_order_t to)
+{
+	size_t i;
+	block_state_t *b;
+
+	debug_assert(from <= BUDDY_MAX_ORDER && to < from, "Invalid orders");
+	for(i = from; i > to; --i)
+	{
+		b = free_list[i];
+		free_list_pop(&free_list[i]);
+		free_list_push(&free_list[i - 1], b);
+		free_list_push(&free_list[i - 1],
+			&blocks_states[GET_BUDDY(BLOCK_ID(b), i)]);
+	}
+}
+
+/*
+ * Coalesces the given block from the given order.
+ */
+static void free_list_coalesce(block_state_t *b, const block_order_t order)
+{
+	size_t i, buddy;
+
+	debug_assert(b, "Invalid argument");
+	debug_assert(order <= BUDDY_MAX_ORDER, "Invalid order");
+	for(i = order; i <= BUDDY_MAX_ORDER; ++i)
+	{
+		debug_assert(blocks_states[BLOCK_ID(b)] == BLOCK_STATE_USED,
+			"Trying to coalesce used block");
+		buddy = GET_BUDDY(BLOCK_ID(b), i);
+		if(buddy >= pages_count || blocks_states[buddy] == BLOCK_STATE_USED)
+			break;
+		// TODO Unlink `b` and `buddy` (buddy might be in the middle of the list)
+	}
+}
+
+/*
  * Allocates a block of memory using the buddy allocator.
  */
 ATTR_HOT
 ATTR_MALLOC
 void *buddy_alloc(const block_order_t order)
 {
+	size_t i;
+	void *ptr;
+
 	errno = 0;
 	if(order > BUDDY_MAX_ORDER)
 		return NULL;
 	spin_lock(&spinlock);
-	// TODO
+	i = order;
+	while(i <= BUDDY_MAX_ORDER && !free_list[i])
+		++i;
+	if(i > BUDDY_MAX_ORDER)
+	{
+		ptr = NULL;
+		goto end;
+	}
+	if(i == order)
+	{
+		free_list_split(i, order);
+		i = order;
+		debug_assert(free_list[i], "Buddy block split fail");
+	}
+	ptr = BLOCK_ID_PTR(BLOCK_ID(free_list[i]));
+	free_list_pop(&free_list[i]);
 	total_allocated_pages += BLOCK_SIZE(order);
+
+end:
 	spin_unlock(&spinlock);
-	return NULL; // TODO
+	return ptr;
 }
 
 /*
@@ -144,13 +243,16 @@ void *buddy_alloc_zero(const block_order_t order)
  * The given order must be the same as the one given to allocate the block.
  */
 ATTR_HOT
-void buddy_free(void *ptr, block_order_t order)
+void buddy_free(void *ptr, const block_order_t order)
 {
 	debug_check_block(buddy_begin, ptr, order);
 	debug_assert(order <= BUDDY_MAX_ORDER,
 		"buddy_free: order > BUDDY_MAX_ORDER");
 	spin_lock(&spinlock);
-	// TODO
+	debug_assert(blocks_states[BLOCK_PTR_ID(ptr)] == BLOCK_STATE_USED,
+		"Freeing unused block");
+	free_list_push(&free_list[order], &blocks_states[BLOCK_PTR_ID(ptr)]);
+	free_list_coalesce(&blocks_states[BLOCK_PTR_ID(ptr)], order);
 	total_allocated_pages -= BLOCK_SIZE(order);
 	spin_unlock(&spinlock);
 }
--- src/memory/buddy/buddy_internal.h
+++ src/memory/buddy/buddy_internal.h
@@ -12,6 +12,27 @@
 # define BLOCK_STATE_USED	((block_state_t) -1)
 
 /*
+ * Returns the id of the block from the pointer to its state.
+ */
+# define BLOCK_ID(state_ptr)	((uintptr_t) ((state_ptr) - blocks_states)\
+	/ sizeof(block_state_t))
+
+/*
+ * Returns the pointer to the page frame from the given id.
+ */
+# define BLOCK_ID_PTR(id)		(buddy_begin + (id) * PAGE_SIZE)
+
+/*
+ * Returns the id to the page frame from the given pointer.
+ */
+# define BLOCK_PTR_ID(ptr)		((uintptr_t) ((ptr) - buddy_begin) / PAGE_SIZE)
+
+/*
+ * Returns the buddy for the given block with the given order.
+ */
+# define GET_BUDDY(id, order)	((id) ^ (order))
+
+/*
  * The type representing the state of a block. Value BLOCK_STATE_USED means used
  * block. Any other value means free block and represents the identifier of the
  * next block into the free list.
