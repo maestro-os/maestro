7812155e06da6224cefdb83c49b9448db105e726
llenotre <llenotre@student.42.fr>
2020-08-17 12:17:03 +0200

    Fixed debug.sh

--- debug.sh
+++ debug.sh
@@ -2,7 +2,7 @@
 
 make
 
-qemu-system-i386 -cdrom maestro.iso -d int -s -S >debug_out 2>&1 &
+setsid qemu-system-i386 -cdrom maestro.iso -d int -s -S >debug_out 2>&1 &
 QEMU_PID=$!
 
 gdb maestro -ex 'target remote :1234'
