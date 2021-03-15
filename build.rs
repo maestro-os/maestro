f0ba9ce16a10fd0460b6ca6e1041171c2365ead9
llenotre <llenotre@student.42.fr>
2021-03-15 13:17:03 +0100

    Changed comments from old format to new format in build.rs

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
-/*
- * This build script allows to link the assembly and C language part of the kernel to the rest of the code.
- */
+///
+/// This build script allows to link the assembly and C language part of the kernel to the rest of the code.
+///
 
-/*
- * The entry of the build script for the kernel.
- */
+///
+/// The entry of the build script for the kernel.
+///
 fn main() {
     println!("cargo:rustc-link-search=native=./");
     println!("cargo:rustc-link-lib=static=maestro");
