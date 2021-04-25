0accf567e0a767020373e6a801a71580a6d32fb6
llenotre <llenotre@student.42.fr>
2021-04-25 17:57:01 +0200

    Added option.rs

--- config/src/main.rs
+++ config/src/main.rs
@@ -1,6 +1,8 @@
 /// The configuration utility allows to easily create configuration files for the kernel's
 /// compilation.
 
+mod option;
+
 use std::io::stdout;
 use std::process;
 
@@ -21,20 +23,62 @@ use crossterm::terminal::LeaveAlternateScreen;
 use crossterm::terminal;
 use crossterm::tty::IsTty;
 
+use option::MenuOption;
+
 /// Structure representing the configuration environment, storage data for rendering and
 /// configuration itself.
 struct ConfigEnv {
+	/// The X position of the cursor.
 	cursor_x: usize,
+	/// The Y position of the cursor.
 	cursor_y: usize,
 
-	// TODO
+	/// The list of available options in the root menu.
+	options: Vec<MenuOption>,
+
+	/// Stores the current menu.
+	current_menu: Vec<String>,
 }
 
 impl ConfigEnv {
+	/// Returns the option with name `name` within the root menu.
+	fn get_root_option(&self, name: &String) -> Option<&MenuOption> {
+		for m in &self.options {
+			if m.name == *name {
+				return Some(&m);
+			}
+		}
+
+		None
+	}
+
+	/// Returns the current menu.
+	fn get_current_menu(&self) -> Option<&MenuOption> {
+		let mut menu: Option<&MenuOption> = None;
+		for m in &self.current_menu {
+			menu = {
+				if let Some(menu_) = menu {
+					menu_.get_suboption(&m)
+				} else {
+					self.get_root_option(&m)
+				}
+			};
+
+			if menu.is_none() {
+				// TODO Error
+				return None;
+			}
+		}
+		menu
+	}
+
 	/// Returns the number of options in the current menu.
-	fn get_options_count(&self) -> usize {
-		// TODO
-		10
+	fn get_current_options(&self) -> &Vec<MenuOption> {
+		if let Some(menu) = &self.get_current_menu() {
+			&menu.suboptions
+		} else {
+			&self.options
+		}
 	}
 
 	/// Returns the number of buttons.
@@ -42,12 +86,55 @@ impl ConfigEnv {
 		// TODO
 		10
 	}
+
+	/// Moves the cursor up.
+	fn move_up(&mut self) {
+        if self.cursor_y > 0 {
+            self.cursor_y -= 1;
+            render_menu(self);
+        }
+	}
+
+	/// Moves the cursor down.
+	fn move_down(&mut self) {
+        if self.cursor_y < self.get_current_options().len() - 1 {
+            self.cursor_y += 1;
+            render_menu(self);
+        }
+	}
+
+	/// Moves the cursor left.
+	fn move_left(&mut self) {
+        if self.cursor_x > 0 {
+            self.cursor_x -= 1;
+            render_menu(self);
+        }
+	}
+
+	/// Moves the cursor right.
+	fn move_right(&mut self) {
+        if self.cursor_x < self.get_buttons_count() - 1{
+            self.cursor_x += 1;
+            render_menu(self);
+        }
+	}
 }
 
-/// Renders the menu.
-fn render_menu(env: &mut ConfigEnv) -> Result<()> {
-	let (width, height) = terminal::size()?;
+/// Renders the `screen too small` error.
+fn render_screen_error() -> Result<()> {
+	execute!(stdout(),
+		SetForegroundColor(Color::Black),
+		SetBackgroundColor(Color::Red))?;
+	execute!(stdout(), Clear(ClearType::All))?;
+	execute!(stdout(), cursor::MoveTo(0, 0))?;
+	println!("Display is too small! (minimum 80x25)");
+	execute!(stdout(), cursor::MoveTo(0, 1))
+}
 
+/// Renders the menu's background and x/y/width/height for the options.
+/// `width` is the width of the terminal.
+/// `height` is the height of the terminal.
+fn render_background(width: u16, height: u16) -> Result<(u16, u16, u16, u16)> {
 	execute!(stdout(),
 		SetForegroundColor(Color::Black),
 		SetBackgroundColor(Color::Blue))?;
@@ -83,24 +170,41 @@ fn render_menu(env: &mut ConfigEnv) -> Result<()> {
 	execute!(stdout(), cursor::MoveTo(menu_x + 2, menu_y + 3))?;
 	print!("<Enter>: Validate");
 
-	execute!(stdout(), cursor::MoveTo(menu_x + 2, menu_y + 5))?;
-	// TODO Render options
-
-	// TODO Render buttons
-
-	execute!(stdout(), cursor::MoveTo(env.cursor_x as _, env.cursor_y as _))
+	let options_x = menu_x + 5;
+	let options_y = menu_y + 5;
+	let options_width = 100; // TODO
+	let options_height = 100; // TODO
+	Ok((options_x, options_y, options_width, options_height))
 }
 
-// TODO rm?
-fn update_selection(env: &mut ConfigEnv) -> Result<()> {
-	// TODO
-	execute!(stdout(), cursor::MoveTo(env.cursor_x as _, env.cursor_y as _))
+/// Renders the menu.
+fn render_menu(env: &mut ConfigEnv) -> Result<()> {
+	let (width, height) = terminal::size()?;
+
+	if width < 80 || height < 25 {
+		render_screen_error()
+	} else {
+		let (opt_x, opt_y, opt_width, opt_height) = render_background(width, height)?;
+		for i in 0..env.get_current_options().len() {
+			execute!(stdout(), cursor::MoveTo(opt_x, opt_y + i as u16))?;
+
+			let option = &env.get_current_options()[i];
+			if option.option_type == "menu" {
+				println!("{}", option.display_name); // TODO
+			}
+		}
+		// TODO Scrolling
+
+		// TODO Render buttons
+
+		execute!(stdout(), cursor::MoveTo(opt_x, opt_y + env.cursor_y as u16))
+	}
 }
 
 /// Resets the terminal before quitting.
-fn reset() {
-	terminal::disable_raw_mode();
-    execute!(stdout(), LeaveAlternateScreen);
+fn reset() -> Result<()> {
+	terminal::disable_raw_mode()?;
+    execute!(stdout(), LeaveAlternateScreen)
 }
 
 /// Waits for a keyboard or resize event.
@@ -110,64 +214,53 @@ fn wait_for_event(env: &mut ConfigEnv) -> Result<()> {
             Event::Key(event) => {
             	match event.code {
             		KeyCode::Up | KeyCode::Char('k') => {
-            			if env.cursor_y > 0 {
-            				env.cursor_y -= 1;
-            				update_selection(env);
-            			}
+            			env.move_up();
             		},
             		KeyCode::Down | KeyCode::Char('j') => {
-            			if env.cursor_y < env.get_options_count() {
-            				env.cursor_y += 1;
-            				update_selection(env);
-            			}
+            			env.move_down();
             		},
             		KeyCode::Left | KeyCode::Char('h') => {
-            			if env.cursor_x > 0 {
-            				env.cursor_x -= 1;
-            				update_selection(env);
-            			}
+            			env.move_left();
             		},
-            		KeyCode::Right | KeyCode::Char('l') => {
-            			if env.cursor_x < env.get_buttons_count() {
-            				env.cursor_x += 1;
-            				update_selection(env);
-            			}
-            		},
-
-            		KeyCode::PageUp => {
-            			// TODO
-            			println!("PageUp");
-            		},
-            		KeyCode::PageDown => {
-            			// TODO
-            			println!("PageDown");
-            		},
-
-            		KeyCode::Char(' ') => {
-            			// TODO
-            			println!("Space");
-            		},
-            		KeyCode::Enter => {
-            			// TODO
-            			println!("Enter");
+            		KeyCode::Right => {
+            			env.move_right();
             		},
 
             		KeyCode::Char('l') => {
             			if event.modifiers == KeyModifiers::CONTROL {
             				render_menu(env);
+            			} else {
+            				env.move_right();
             			}
+            		},
+
+            		KeyCode::PageUp => {
+            			// TODO
+            		},
+            		KeyCode::PageDown => {
+            			// TODO
+            		},
+
+            		KeyCode::Char(' ') => {
+            			// TODO
+            		},
+            		KeyCode::Enter => {
+            			// TODO
             		},
 
             		KeyCode::Esc => {
             			// TODO Ask for confirmation
-						reset();
+						reset()?;
             			process::exit(0);
             		},
 
             		_ => {},
             	}
             },
-            Event::Resize(width, height) => println!("New size {}x{}", width, height), // TODO
+
+            Event::Resize(_, _) => {
+            	render_menu(env);
+            },
 
             _ => {},
         }
@@ -175,20 +268,23 @@ fn wait_for_event(env: &mut ConfigEnv) -> Result<()> {
 }
 
 /// Displays the configuration utility.
-fn display() -> Result<()> {
+fn display(options: Vec<MenuOption>) -> Result<()> {
 	execute!(stdout(), EnterAlternateScreen)?;
-	terminal::enable_raw_mode();
+	terminal::enable_raw_mode()?;
 
 	let mut env = ConfigEnv {
 		cursor_x: 0,
 		cursor_y: 0,
+
+		options: options,
+
+		current_menu: Vec::new(),
 	};
 
 	render_menu(&mut env);
     wait_for_event(&mut env)?;
 
-	reset();
-	Ok(())
+	reset()
 }
 
 fn main() {
@@ -212,7 +308,14 @@ tool");
 		process::exit(1);
 	}
 
-	if display().is_err() {
+	let options_results = option::from_file("config_options.json");
+	if let Err(err) = options_results {
+		eprintln!("{}", err);
+		process::exit(1);
+	}
+
+	let options = options_results.unwrap();
+	if display(options).is_err() {
 		eprintln!("Terminal error!");
 		process::exit(1);
 	}
---
+++ config/src/option.rs
@@ -0,0 +1,56 @@
+/// This module implements the MenuOption structure and its parsing from a JSON file.
+/// An option can either be a menu, or a configuration option to select.
+
+use std::fs;
+
+use serde::Deserialize;
+
+/// Structure representing an option.
+#[derive(Deserialize)]
+pub struct MenuOption {
+	/// The identifier of the option.
+	pub name: String,
+	/// The display name of the option.
+	pub display_name: String,
+	/// The description of the option.
+	pub desc: String,
+	/// The type of the option.
+	pub option_type: String,
+	/// The set of possible values.
+	pub values: Vec<String>,
+	/// The default value.
+	pub default: String,
+	/// The set of dependencies needed to enable this option. TODO: document dependency name
+	pub deps: Vec<String>,
+	/// The set of suboptions.
+	pub suboptions: Vec<MenuOption>,
+}
+
+impl MenuOption {
+	/// Returns the option with name `name` within the current menu.
+	pub fn get_suboption(&self, name: &String) -> Option<&MenuOption> {
+		for m in &self.suboptions {
+			if m.name == *name {
+				return Some(&m);
+			}
+		}
+
+		None
+	}
+}
+
+/// Reads all options from the given JSON file `file`.
+pub fn from_file(file: &str) -> Result<Vec<MenuOption>, &'static str> {
+	if let Ok(data) = fs::read_to_string(file) {
+		let options_result = serde_json::from_str(&data);
+		if let Err(err) = options_result {
+			eprintln!("{}", err); // TODO Move?
+			Err("Failed to parse options file!")
+		} else {
+			// TODO Check for dependencies cycle
+			Ok(options_result.unwrap())
+		}
+	} else {
+		Err("Failed to open options file!")
+	}
+}
