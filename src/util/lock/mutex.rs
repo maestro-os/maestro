72ec50bba94aba1eddd7299a6cac252b65241ad8
llenotre <llenotre@student.42.fr>
2021-05-06 22:29:34 +0200

    Removed useless LockPayload

--- src/util/lock/mutex.rs
+++ src/util/lock/mutex.rs
@@ -2,41 +2,50 @@
 
 use crate::util::lock::spinlock::Spinlock;
 
-/// This structure is used to give access to a payload owned by a concurrency control structure.
-pub struct LockPayload<'a, T> {
-	/// A mutable reference to the Mutex.
-	mutex: &'a mut Mutex::<T>,
+/// Type used to declare a guard meant to unlock the associated Mutex at the moment the execution
+/// gets out of the scope of its declaration. This structure is useful to ensure that the mutex
+/// doesen't stay locked after the exectution of a function ended.
+pub struct MutexGuard<'a, T> {
+	/// The mutex associated to the guard
+	mutex: &'a mut Mutex<T>,
 }
 
-impl<'a, T> LockPayload<'a, T> {
-	/// Creates a new lock payload instance.
-	pub fn new(mutex: &'a mut Mutex::<T>) -> Self {
-		Self {
+impl<'a, T> MutexGuard<'a, T> {
+	/// Creates an instance of MutexGuard for the given `mutex` and locks it.
+	pub fn new(mutex: &'a mut Mutex<T>) -> Self {
+		let g = Self {
 			mutex: mutex,
-		}
+		};
+		g.mutex.spin.lock();
+		g
 	}
 
-	/// Gives access to the payload.
+	/// Returns an immutable reference to the data owned by the associated Mutex.
 	pub fn get(&self) -> &T {
-		&self.mutex.data
-	}
-
-	/// Gives access to the payload.
-	pub fn get_mut(&mut self) -> &mut T {
-		&mut self.mutex.data
-	}
-}
-
-impl<'a, T> Drop for LockPayload<'a, T> {
-	/// Called when the LockPayload gets out of the scope of execution.
-	fn drop(&mut self) {
 		unsafe {
-			self.mutex.unlock();
+			self.mutex.get_payload()
 		}
 	}
+
+	/// Returns a mutable reference to the data owned by the associated Mutex.
+	pub fn get_mut(&mut self) -> &mut T {
+		unsafe {
+			self.mutex.get_mut_payload()
+		}
+	}
+
+	/// Unlocks the Mutex.
+	pub fn unlock(self) {}
 }
 
-/// Mutual exclusion for protection of sensitive data.
+impl<'a, T> Drop for MutexGuard<'a, T> {
+	/// Called when the MutexGuard gets out of the scope of execution.
+	fn drop(&mut self) {
+		self.mutex.spin.unlock();
+	}
+}
+
+/// Mutual exclusion, used to protect data from concurrent access.
 /// A Mutex allows to ensure that one, and only thread accesses the data stored into it at the same
 /// time. Preventing race conditions.
 /// This structure works using spinlocks.
@@ -65,14 +74,9 @@ impl<T> Mutex<T> {
 
 	/// Locks the mutex. If the mutex is already locked, the thread shall wait until it becomes
 	/// available.
-	pub fn lock(&mut self) -> LockPayload<T> {
-		self.spin.lock();
-		LockPayload::<T>::new(self)
-	}
-
-	/// Unlocks the Mutex.
-	unsafe fn unlock(&mut self) {
-		self.spin.unlock();
+	/// The function returns a MutexGuard associated with the Mutex.
+	pub fn lock(&mut self) -> MutexGuard<T> {
+		MutexGuard::new(self)
 	}
 
 	/// Returns an immutable reference to the payload. This function is unsafe because it can return
@@ -85,47 +89,5 @@ impl<T> Mutex<T> {
 	/// the payload while the Mutex isn't locked.
 	pub unsafe fn get_mut_payload(&mut self) -> &mut T {
 		&mut self.data
-	}
-}
-
-/// Type used to declare a guard meant to unlock the associated Mutex at the moment the execution
-/// gets out of the scope of its declaration. This structure is useful to ensure that the mutex
-/// doesen't stay locked after the exectution of a function ended.
-pub struct MutexGuard<'a, T> {
-	/// The mutex associated to the guard
-	mutex: &'a mut Mutex<T>,
-}
-
-impl<'a, T> MutexGuard<'a, T> {
-	/// Creates an instance of MutexGuard for the given `mutex` and locks it.
-	pub fn new(mutex: &'a mut Mutex<T>) -> Self {
-		let g = Self {
-			mutex: mutex,
-		};
-		g.mutex.lock();
-		g
-	}
-
-	/// Returns an immutable reference to the data owned by the associated Mutex.
-	pub fn get(&self) -> &T {
-		unsafe {
-			self.mutex.get_payload()
-		}
-	}
-
-	/// Returns a mutable reference to the data owned by the associated Mutex.
-	pub fn get_mut(&mut self) -> &mut T {
-		unsafe {
-			self.mutex.get_mut_payload()
-		}
-	}
-}
-
-impl<'a, T> Drop for MutexGuard<'a, T> {
-	/// Called when the MutexGuard gets out of the scope of execution.
-	fn drop(&mut self) {
-		unsafe {
-			self.mutex.unlock();
-		}
 	}
 }
