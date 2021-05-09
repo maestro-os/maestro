86341ecde6a19e2ef9ed085f69bc2859c650940d
llenotre <llenotre@student.42.fr>
2021-05-09 19:48:57 +0200

    Implemented hash map

--- src/util/container/hashmap.rs
+++ src/util/container/hashmap.rs
@@ -5,6 +5,9 @@ use core::hash::Hasher;
 use core::mem::size_of_val;
 use core::ops::Index;
 use core::ops::IndexMut;
+use core::ptr;
+use crate::errno::Errno;
+use crate::errno;
 use super::vec::Vec;
 
 /// Bitwise XOR hasher.
@@ -45,6 +48,64 @@ struct Bucket<K: Eq + Hash, V> {
 	elements: Vec<(K, V)>,
 }
 
+impl<K: Eq + Hash, V> Bucket<K, V> {
+	/// Creates a new instance.
+	fn new() -> Self {
+		Self {
+			elements: Vec::new(),
+		}
+	}
+
+	/// Returns an immutable reference to the value with the given key `k`. If the key isn't
+	/// present, the function return None.
+	pub fn get(&self, k: K) -> Option<&V> {
+		for i in 0..self.elements.len() {
+			if self.elements[i].0 == k {
+				return Some(&self.elements[i].1);
+			}
+		}
+
+		None
+	}
+
+	/// Returns a mutable reference to the value with the given key `k`. If the key isn't present,
+	/// the function return None.
+	pub fn get_mut(&mut self, k: K) -> Option<&mut V> {
+		for i in 0..self.elements.len() {
+			if self.elements[i].0 == k {
+				return Some(&mut self.elements[i].1);
+			}
+		}
+
+		None
+	}
+
+	/// Inserts a new element into the bucket. If the key was already present, the function
+	/// returns the previous value.
+	pub fn insert(&mut self, k: K, v: V) -> Result<Option<V>, Errno> {
+		let old = self.remove(&k);
+		self.elements.push((k, v))?;
+		Ok(old)
+	}
+
+	/// Removes an element from the bucket. If the key was present, the function returns the
+	/// value.
+	pub fn remove(&mut self, k: &K) -> Option<V> {
+		for i in 0..self.elements.len() {
+			if self.elements[i].0 == *k {
+				let val = unsafe {
+					ptr::read(&self.elements[i].1 as _)
+				};
+				self.elements.remove(i);
+
+				return Some(val);
+			}
+		}
+
+		None
+	}
+}
+
 /// Structure representing a hashmap.
 pub struct HashMap<K: Eq + Hash, V> {
 	/// The vector containing buckets.
@@ -53,43 +114,86 @@ pub struct HashMap<K: Eq + Hash, V> {
 
 impl<K: Eq + Hash, V> HashMap::<K, V> {
 	/// Creates a new instance.
-	pub fn new() -> Self {
-		Self {
-			buckets: Vec::new(),
+	pub fn new(buckets_count: usize) -> Result<Self, Errno> {
+		if buckets_count == 0 {
+			return Err(errno::EINVAL);
 		}
+
+		let mut map = Self {
+			buckets: Vec::with_capacity(buckets_count)?,
+		};
+		for _ in 0..buckets_count {
+			map.buckets.push(Bucket::new())?;
+		}
+		Ok(map)
+	}
+
+	/// Returns the number of elements in the hash map.
+	pub fn len(&self) -> usize {
+		let mut total = 0;
+
+		for b in self.buckets.iter() {
+			total += b.elements.len();
+		}
+
+		total
+	}
+
+	/// Tells whether the hash map is empty.
+	pub fn is_empty(&self) -> bool {
+		self.len() == 0
+	}
+
+	/// Returns the number of buckets.
+	pub fn get_buckets_count(&self) -> usize {
+		self.buckets.len()
+	}
+
+	/// Returns the bucket index for the key `k`.
+	fn get_bucket_index(&self, k: &K) -> usize {
+		let mut hasher = XORHasher::new();
+		k.hash(&mut hasher);
+		(hasher.finish() / (self.buckets.len() as u64)) as usize
 	}
 
 	/// Returns an immutable reference to the value with the given key `k`. If the key isn't
 	/// present, the function return None.
-	pub fn get(&self, _k: K) -> Option<&V> {
-		// TODO
-		None
+	pub fn get(&self, k: K) -> Option<&V> {
+		let index = self.get_bucket_index(&k);
+		self.buckets[index].get(k)
 	}
 
 	/// Returns a mutable reference to the value with the given key `k`. If the key isn't present,
 	/// the function return None.
-	pub fn get_mut(&mut self, _k: K) -> Option<&mut V> {
-		// TODO
-		None
+	pub fn get_mut(&mut self, k: K) -> Option<&mut V> {
+		let index = self.get_bucket_index(&k);
+		self.buckets[index].get_mut(k)
+	}
+
+	/// Creates an iterator for the hash map.
+	pub fn iter(&self) -> HashMapIterator<K, V> {
+		HashMapIterator::new(self)
 	}
 
 	/// Inserts a new element into the hash map. If the key was already present, the function
 	/// returns the previous value.
-	pub fn insert(&mut self, _k: K, _v: V) -> Option<V> {
-		// TODO
-		None
+	pub fn insert(&mut self, k: K, v: V) -> Result<Option<V>, Errno> {
+		let index = self.get_bucket_index(&k);
+		self.buckets[index].insert(k, v)
 	}
 
 	/// Removes an element from the hash map. If the key was present, the function returns the
 	/// value.
-	pub fn remove(&mut self, _k: K, _v: V) -> Option<V> {
-		// TODO
-		None
+	pub fn remove(&mut self, k: K) -> Option<V> {
+		let index = self.get_bucket_index(&k);
+		self.buckets[index].remove(&k)
 	}
 
 	/// Drops all elements in the hash map.
 	pub fn clear(&mut self) {
-		// TODO
+		for i in 0..self.buckets.len() {
+			self.buckets[i].elements.clear();
+		}
 	}
 }
 
@@ -109,14 +213,110 @@ impl<K: Eq + Hash, V> IndexMut<K> for HashMap<K, V> {
 	}
 }
 
-// TODO Iterator
+/// An iterator for the Vec structure.
+pub struct HashMapIterator<'a, K: Hash + Eq, V> {
+	/// The hash map to iterate into.
+	hm: &'a HashMap<K, V>,
+
+	/// The current bucket index.
+	curr_bucket: usize,
+	/// The current element index.
+	curr_element: usize,
+}
+
+impl<'a, K: Hash + Eq, V> HashMapIterator<'a, K, V> {
+	/// Creates a hash map iterator for the given reference.
+	fn new(hm: &'a HashMap<K, V>) -> Self {
+		Self {
+			hm: hm,
+
+			curr_bucket: 0,
+			curr_element: 0,
+		}
+	}
+}
+
+impl<'a, K: Hash + Eq, V> Iterator for HashMapIterator<'a, K, V> {
+	type Item = &'a V;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		if self.curr_bucket >= self.hm.buckets.len() {
+			return None;
+		}
+
+		let v = &self.hm.buckets[self.curr_bucket].elements[self.curr_element].1;
+
+		if self.curr_element >= self.hm.buckets[self.curr_bucket].elements.len() {
+			self.curr_element = 0;
+
+			for i in (self.curr_bucket + 1)..self.hm.buckets.len() {
+				if !self.hm.buckets[i].elements.is_empty() {
+					self.curr_bucket = i;
+					break;
+				}
+			}
+
+			if self.curr_bucket >= self.hm.buckets.len() {
+				return None
+			}
+		}
+
+		Some(v)
+	}
+
+	fn count(self) -> usize {
+		self.hm.len()
+	}
+}
 
 #[cfg(test)]
 mod test {
 	use super::*;
 
 	#[test_case]
-	fn hashmap0() {
-		// TODO
+	fn hash_map0() {
+		if let Err(e) = HashMap::<u32, u32>::new(0) {
+			assert_eq!(e, errno::EINVAL);
+		} else {
+			assert!(false);
+		}
+	}
+
+	#[test_case]
+	fn hash_map1() {
+		let mut hash_map = HashMap::<u32, u32>::new(16).unwrap();
+
+		assert_eq!(hash_map.len(), 0);
+
+		hash_map.insert(0, 0).unwrap();
+
+		assert_eq!(hash_map.len(), 1);
+		assert_eq!(*hash_map.get(0).unwrap(), 0);
+		assert_eq!(hash_map[0], 0);
+
+		assert_eq!(hash_map.remove(0).unwrap(), 0);
+
+		assert_eq!(hash_map.len(), 0);
+	}
+
+	#[test_case]
+	fn hash_map2() {
+		let mut hash_map = HashMap::<u32, u32>::new(16).unwrap();
+
+		for i in 0..100 {
+			assert_eq!(hash_map.len(), i);
+
+			hash_map.insert(i as _, 0).unwrap();
+
+			assert_eq!(hash_map.len(), i + 1);
+			assert_eq!(*hash_map.get(i as _).unwrap(), 0);
+			assert_eq!(hash_map[i as _], 0);
+		}
+
+		for i in (0..100).rev() {
+			assert_eq!(hash_map.len(), i + 1);
+			assert_eq!(hash_map.remove(i as _).unwrap(), 0);
+			assert_eq!(hash_map.len(), i);
+		}
 	}
 }
