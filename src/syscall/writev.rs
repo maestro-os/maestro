4234b643b30ac60c0721d7e74ae27bf77c01efa5
llenotre <llenotre@student.42.fr>
2021-12-16 18:22:33 +0100

    Added memory access checking in writev

--- src/syscall/writev.rs
+++ src/syscall/writev.rs
@@ -1,9 +1,11 @@
 //! TODO doc
 
 use core::cmp::min;
+use core::mem::size_of;
 use core::slice;
 use crate::errno::Errno;
 use crate::errno;
+use crate::limits;
 use crate::process::Process;
 use crate::process::Regs;
 use crate::process::iovec::IOVec;
@@ -14,26 +16,44 @@ pub fn writev(regs: &Regs) -> Result<i32, Errno> {
 	let iov = regs.ecx as *const IOVec;
 	let iovcnt = regs.edx as i32;
 
+	// Checking the size of the vector is in bounds
+	if iovcnt < 0 || iovcnt as usize > limits::IOV_MAX {
+		return Err(errno::EINVAL);
+	}
+
 	let mutex = Process::get_current().unwrap();
 	let mut guard = mutex.lock(false);
 	let proc = guard.get_mut();
 
-	// TODO Check access for each
+	// Checking that the vector is accessible
+	if !proc.get_mem_space().unwrap().can_access(iov as *const _,
+		iovcnt as usize * size_of::<IOVec>(), true, false) {
+		return Err(errno::EFAULT);
+	}
+
+	let iov_slice = unsafe { // Safe because the access is checked before
+		slice::from_raw_parts(iov, iovcnt as _)
+	};
+
+	// TODO Compute total length, then check it is in bound
+	// Checking access to each buffers
+	for i in iov_slice {
+		if !proc.get_mem_space().unwrap().can_access(i.iov_base as *const _, i.iov_len, true,
+			false) {
+			return Err(errno::EFAULT);
+		}
+	}
 
 	let fd = proc.get_fd(fd).ok_or(errno::EBADF)?;
 
-	// The total written length
 	let mut total_len = 0;
-	for i in 0..iovcnt {
-		// The IO vector
-		let iov = unsafe { // Safe because the access is checked before
-			&*iov.add(i as _)
-		};
+
+	for i in iov_slice {
 		// The size to write. This is limited to avoid an overflow on the total length
-		let l = min(iov.iov_len, i32::MAX as usize - total_len);
+		let l = min(i.iov_len, i32::MAX as usize - total_len);
 		// The slice on the data
 		let slice = unsafe { // Safe because the access is checked before
-			slice::from_raw_parts(iov.iov_base as *const u8, l)
+			slice::from_raw_parts(i.iov_base as *const u8, l)
 		};
 
 		total_len += fd.write(slice)?;
