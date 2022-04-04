12aa032d05f9f2c8c8bda707abc14e50b0a4e088
llenotre <llenotre@student.42.fr>
2022-04-05 00:32:01 +0200

    Prepared arguments retrieval for the clone system call

--- src/syscall/clone.rs
+++ src/syscall/clone.rs
@@ -1,10 +1,17 @@
 //! The `clone` system call creates a child process.
 
+use core::ffi::c_void;
 use crate::errno::Errno;
 use crate::process::regs::Regs;
 
 /// The implementation of the `clone` syscall.
-pub fn clone(_regs: &Regs) -> Result<i32, Errno> {
+pub fn clone(regs: &Regs) -> Result<i32, Errno> {
+	let _flags = regs.ebx as i32;
+	let _stack = regs.ecx as *mut c_void;
+	let _parent_tid = regs.edx as *mut i32;
+	let _tls = regs.esi as i32;
+	let _child_tid = regs.edi as *mut i32;
+
 	// TODO
 	todo!();
 }
