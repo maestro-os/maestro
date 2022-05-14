f566e7de8f208d70d638634bedce7263af7f7e11
llenotre <llenotre@student.42.fr>
2022-05-15 01:26:06 +0200

    Added killing process with SIGPIPE when writing on a broken pipe

--- src/syscall/write.rs
+++ src/syscall/write.rs
@@ -7,6 +7,8 @@ use crate::file::open_file::O_NONBLOCK;
 use crate::process::Process;
 use crate::process::mem_space::ptr::SyscallSlice;
 use crate::process::regs::Regs;
+use crate::process::signal;
+use crate::syscall::Signal;
 
 // TODO O_ASYNC
 
@@ -39,7 +41,24 @@ pub fn write(regs: &Regs) -> Result<i32, Errno> {
 			let open_file = open_file_guard.get_mut();
 
 			let flags = open_file.get_flags();
-			(open_file.write(buf_slice)?, flags) // TODO On EPIPE, kill current with SIGPIPE
+			let len = match open_file.write(buf_slice) {
+				Ok(len) => len,
+
+				Err(err) => {
+					// If the pipe is broken, kill with SIGPIPE
+					if err.as_int() == errno::EPIPE {
+						let mutex = Process::get_current().unwrap();
+						let mut guard = mutex.lock();
+						let proc = guard.get_mut();
+
+						proc.kill(Signal::new(signal::SIGPIPE).unwrap(), false);
+					}
+
+					return Err(err);
+				},
+			};
+
+			(len, flags)
 		};
 
 		// TODO Continue until everything was written?
