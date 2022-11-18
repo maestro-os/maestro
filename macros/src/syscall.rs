c3d07fc1acd0297da8933485020c0cc53a14ba7f
llenotre <llenotre@student.42.fr>
2022-11-18 01:26:58 +0100

    Implemented syscall macro

--- macros/src/syscall.rs
+++ macros/src/syscall.rs
@@ -1,30 +1,94 @@
 //! This module implements the macro used to declare a system call.
 
+use proc_macro2::Ident;
+use proc_macro2::Span;
 use proc_macro::TokenStream;
-use syn::Expr;
-use syn::ExprCall;
+use quote::quote;
+use syn::FnArg;
 use syn::ItemFn;
-use syn::Stmt;
 use syn::parse_macro_input;
+
+/// The list of register for each argument, in order.
+const REGS: [&'static str; 6] = [
+	"ebx",
+	"ecx",
+	"edx",
+	"esi",
+	"edi",
+	"ebp"
+];
 
 /// Implementation of the syscall macro.
 pub fn syscall(input: TokenStream) -> TokenStream {
 	let input = parse_macro_input!(input as ItemFn);
 
-	match input.block.stmts.first() {
-		Some(Stmt::Semi(Expr::Call(ExprCall {
-			args,
-			..
-		}), _)) => {
-			for _arg in args {
-				// TODO
-				eprintln!("arg");
-			}
-		},
-
-		_ => panic!(), // TODO message
+	// Check signature is valid
+	if input.sig.constness.is_some() {
+		panic!("a system call handler cannot be const");
+	}
+	if !input.sig.generics.params.is_empty() {
+		panic!("a system call cannot have generic arguments");
+	}
+	if input.sig.variadic.is_some() {
+		panic!("a system call handler cannot have variadic arguments");
+	}
+	if input.sig.inputs.len() >= REGS.len() {
+		panic!("too many arguments for the current target (max: {})", REGS.len());
 	}
 
-	// TODO
-	todo!();
+	let mut args = proc_macro2::TokenStream::new();
+	args.extend(input.sig.inputs.iter()
+		.enumerate()
+		.map(|(i, arg)| match arg {
+			FnArg::Typed(typed) => {
+				let pat = typed.pat.clone();
+				let ty = typed.ty.clone();
+
+				// TODO Check type is supported
+				// Supported list:
+				// - c_char
+				// - c_double
+				// - c_float
+				// - c_int
+				// - c_long
+				// - c_longlong
+				// - c_schar
+				// - c_short
+				// - c_uchar
+				// - c_uint
+				// - c_ulong
+				// - c_ulonglong
+				// - c_ushort
+				// - c_void
+				// - u8
+				// - i8
+				// - u16
+				// - i16
+				// - u32
+				// - i32
+				// - u64
+				// - i64
+				// - all pointers
+				// - Syscall*
+
+				let reg_name = Ident::new(REGS[i], Span::call_site());
+
+				proc_macro2::TokenStream::from(quote! {
+					let #pat = regs.#reg_name as #ty;
+				})
+			},
+
+			FnArg::Receiver(_) => panic!("a system call handler cannot have a `self` argument"),
+		}));
+
+	let ident = input.sig.ident;
+	let code = input.block;
+
+	TokenStream::from(quote! {
+		pub fn #ident(regs: &crate::process::regs::Regs) -> Result<i32, Errno> {
+			#args
+
+			#code
+		}
+	})
 }
