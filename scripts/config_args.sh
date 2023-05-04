4a66875e43edcbe4f12891f641eb91a14c627f2d
llenotre <llenotre@student.42.fr>
2023-05-05 00:00:59 +0200

    Removed legacy scripts

--- scripts/config_args.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# This script takes the configuration of the kernel from the configuration file and to turns it into command line arguments to pass to the compiler
-
-cat .config 2>/dev/null | tr '[:upper:]' '[:lower:]' | grep -v '="false"$' | sed 's/^/--cfg config_/' | sed 's/="true"$//' | tr '\n' ' '
