63d959d1109f5fd54abae987ce7415356e5f47c1
llenotre <llenotre@student.42.fr>
2023-05-21 18:24:45 +0200

    Fixing CI selftests

--- scripts/ci/selftest.sh
+++ scripts/ci/selftest.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+export QEMU_FLAGS="-serial file:serial.log"
+
+cp default.config.toml config.toml
+sed -i 's/^qemu = false$/qemu = true/' config.toml
+
 echo "Running selftests..."
-# TODO
+cargo run
 
 echo "Selftests output:"
 cat serial.log
