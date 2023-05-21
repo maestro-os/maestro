55a55cc14c5e1481923df8fe805a7d406a72823c
llenotre <llenotre@student.42.fr>
2023-05-21 20:50:03 +0200

    CI WIP

--- scripts/ci/selftest.sh
+++ scripts/ci/selftest.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export QEMU_FLAGS="-nographic -serial file:serial.log -serial mon:null -device isa-debug-exit,iobase=0xf4,iosize=0x04"
+export QEMU_FLAGS="-nographic -serial none -serial file:serial.log -device isa-debug-exit,iobase=0xf4,iosize=0x04"
 
 cp default.config.toml config.toml
 sed -i 's/^qemu = false$/qemu = true/' config.toml
@@ -22,7 +22,7 @@ set -e
 
 echo
 echo "Selftests output:"
-cat serial.log
+cat -e serial.log
 
 if [ "$EXIT" -ne 33 ]; then
 	exit 1
