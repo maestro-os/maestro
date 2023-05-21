614715bb6b210371146cf66a91a2d9e5112446af
llenotre <llenotre@student.42.fr>
2023-05-21 18:42:51 +0200

    CI WIP

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -7,7 +7,7 @@ EXIT_CODE=0
 for arch in $(ls -1 arch/); do
 	echo "Build for architecture $arch..."
 	cargo build --target arch/$arch/$arch.json $CARGOFLAGS
-	EXIT_CODE=$(($EXIT_CODE + 1))
+	EXIT_CODE=$(($EXIT_CODE + $?))
 done
 
 exit $EXIT_CODE
