ee0edcd77a5af8feaa8ba8fff5cf85f81f253f28
llenotre <llenotre@student.42.fr>
2023-05-21 18:38:01 +0200

    CI WIP

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -2,7 +2,12 @@
 
 cp default.config.toml config.toml
 
+EXIT_CODE=0
+
 for arch in $(ls -1 arch/); do
 	echo "Build for architecture $arch..."
-	cargo build --target arch/$arch/$arch.json
+	cargo build --target arch/$arch/$arch.json $CARGOFLAGS
+	EXIT_CODE=$(($EXIT_CODE + 1))
 done
+
+exit $EXIT_CODE
--- scripts/ci/selftest.sh
+++ scripts/ci/selftest.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+set -e
+
 export QEMU_FLAGS="-serial file:serial.log"
 
 cp default.config.toml config.toml
 sed -i 's/^qemu = false$/qemu = true/' config.toml
 
 echo "Running selftests..."
-cargo run
+cargo test
 
 echo "Selftests output:"
 cat serial.log
