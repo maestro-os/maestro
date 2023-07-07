b7c91b044371b120e769ac079fd8a38afaa24564
llenotre <llenotre@student.42.fr>
2023-07-07 21:52:34 +0200

    Using `ClockIdT` in syscalls

--- src/syscall/clock_gettime.rs
+++ src/syscall/clock_gettime.rs
@@ -4,12 +4,13 @@ use crate::errno::Errno;
 use crate::process::mem_space::ptr::SyscallPtr;
 use crate::process::Process;
 use crate::time;
+use crate::time::unit::ClockIdT;
 use crate::time::unit::Timespec;
 use macros::syscall;
 
 // TODO Check first arg
 #[syscall]
-pub fn clock_gettime(_clock_id: i32, tp: SyscallPtr<Timespec>) -> Result<i32, Errno> {
+pub fn clock_gettime(_clockid: ClockIdT, tp: SyscallPtr<Timespec>) -> Result<i32, Errno> {
 	// TODO Get clock according to param
 	let clk = b"TODO";
 	let curr_time = time::get_struct::<Timespec>(clk, true).ok_or(errno!(EINVAL))?;
