759266f5baa2ede3e00fd0352756808053d6db8f
llenotre <llenotre@student.42.fr>
2023-12-16 14:08:57 +0100

    fix(getdents*): entries alignment in memory

--- src/syscall/getdents.rs
+++ src/syscall/getdents.rs
@@ -1,35 +1,37 @@
 //! The `getdents` system call allows to get the list of entries in a given
 //! directory.
 
-use crate::errno::Errno;
-use crate::file::FileContent;
+use crate::errno::{EResult, Errno};
+use crate::file::{FileContent, FileType, INode};
 use crate::process::mem_space::ptr::SyscallSlice;
 use crate::process::Process;
 use core::ffi::c_uint;
-use core::ffi::c_void;
 use core::mem::offset_of;
 use core::mem::size_of;
 use core::ptr;
 use macros::syscall;
 
-/// Structure representing a Linux directory entry.
-#[repr(C)]
-struct LinuxDirent {
-	/// Inode number.
-	d_ino: u32,
-	/// Offset to the next entry.
-	d_off: u32,
-	/// Length of this entry.
-	d_reclen: u16,
-	/// Filename (null-terminated).
+/// A directory entry as returned by the `getdents*` system calls.
+pub trait Dirent: Sized {
+	/// Returns the number of bytes required for an entry with the given name.
 	///
-	/// The filename is immediately followed by a zero padding byte, then a byte
-	/// indicating the type of the entry.
-	d_name: [u8; 0],
+	/// This function must return a number that ensures the entry is aligned in memory (a multiple
+	/// of `4` or `8` depending on the architecture).
+	fn required_length(name: &[u8]) -> usize;
+
+	/// Writes a new entry on the given slice.
+	///
+	/// Arguments:
+	/// - `slice` is the slice to write on.
+	/// - `off` is the offset to the beginning of the entry in the slice.
+	/// - `inode` is the inode of the entry.
+	/// - `entry_type` is the type of the entry.
+	/// - `name` is the name of the entry.
+	fn write(slice: &mut [u8], off: usize, inode: INode, entry_type: FileType, name: &[u8]);
 }
 
-#[syscall]
-pub fn getdents(fd: c_uint, dirp: SyscallSlice<c_void>, count: c_uint) -> Result<i32, Errno> {
+/// Performs the getdents system call.
+pub fn do_getdents<E: Dirent>(fd: c_uint, dirp: SyscallSlice<u8>, count: usize) -> EResult<i32> {
 	let (mem_space, open_file_mutex) = {
 		let proc_mutex = Process::current_assert();
 		let proc = proc_mutex.lock();
@@ -48,68 +50,40 @@ pub fn getdents(fd: c_uint, dirp: SyscallSlice<c_void>, count: c_uint) -> Result
 		(mem_space, open_file_mutex)
 	};
 
-	// Getting file
-	let mut open_file = open_file_mutex.lock();
-
 	let mut mem_space_guard = mem_space.lock();
 	let dirp_slice = dirp
 		.get_mut(&mut mem_space_guard, count as _)?
 		.ok_or_else(|| errno!(EFAULT))?;
 
+	let mut open_file = open_file_mutex.lock();
+	let start = open_file.get_offset();
+
 	let mut off = 0;
 	let mut entries_count = 0;
-	let start = open_file.get_offset();
 
 	{
 		let file_mutex = open_file.get_file();
 		let file = file_mutex.lock();
 
-		let entries = match file.get_content() {
-			FileContent::Directory(entries) => entries,
-			_ => return Err(errno!(ENOTDIR)),
+		let FileContent::Directory(entries) = file.get_content() else {
+			return Err(errno!(ENOTDIR));
 		};
+		// TODO skip entries whose inode cannot fit in struct
+		let entries = entries.iter().skip(start as _);
 
-		// Iterating over entries and filling the buffer
-		for (name, entry) in entries.iter().skip(start as _) {
-			// Skip entries if the inode cannot fit
-			if entry.inode > u32::MAX as _ {
-				continue;
-			}
-
-			let len = size_of::<LinuxDirent>() + name.len() + 2;
+		// Iterate over entries and fill the buffer
+		for (name, entry) in entries {
+			let len = E::required_length(name);
 			// If the buffer is not large enough, return an error
-			if off == 0 && len > count as usize {
+			if off == 0 && len > count {
 				return Err(errno!(EINVAL));
 			}
 			// If reaching the end of the buffer, break
-			if off + len > count as usize {
+			if off + len > count {
 				break;
 			}
 
-			let ent_ptr = &mut dirp_slice[off] as *mut _ as *mut LinuxDirent;
-			let ent = LinuxDirent {
-				d_ino: entry.inode as _,
-				d_off: (off + len) as _,
-				d_reclen: len as _,
-				d_name: [],
-			};
-			let ent_name_ptr =
-				unsafe { (ent_ptr as *mut u8).add(offset_of!(LinuxDirent, d_name)) };
-
-			unsafe {
-				// Writing entry
-				ptr::write_unaligned(ent_ptr, ent);
-
-				// Copying file name
-				ptr::copy_nonoverlapping(name.as_bytes().as_ptr(), ent_name_ptr, name.len());
-
-				// Writing padding byte
-				*ent_name_ptr.add(name.len()) = 0;
-
-				// Writing entry type
-				let entry_type = entry.entry_type.to_dirent_type();
-				*ent_name_ptr.add(name.len() + 1) = entry_type;
-			}
+			E::write(dirp_slice, off, entry.inode, entry.entry_type, name);
 
 			off += len;
 			entries_count += 1;
@@ -118,4 +92,54 @@ pub fn getdents(fd: c_uint, dirp: SyscallSlice<c_void>, count: c_uint) -> Result
 
 	open_file.set_offset(start + entries_count);
 	Ok(off as _)
+}
+
+/// Structure representing a Linux directory entry.
+#[repr(C)]
+struct LinuxDirent {
+	/// Inode number.
+	d_ino: u32,
+	/// Offset to the next entry.
+	d_off: u32,
+	/// Length of this entry.
+	d_reclen: u16,
+	/// Filename (nul-terminated).
+	///
+	/// The filename is immediately followed by a zero padding byte, then a byte
+	/// indicating the type of the entry.
+	d_name: [u8; 0],
+}
+
+impl Dirent for LinuxDirent {
+	fn required_length(name: &[u8]) -> usize {
+		(size_of::<Self>() + name.len() + 2)
+			// Padding for alignment
+			.next_multiple_of(size_of::<usize>())
+	}
+
+	fn write(slice: &mut [u8], off: usize, inode: INode, entry_type: FileType, name: &[u8]) {
+		let len = Self::required_length(name);
+		let ent = Self {
+			d_ino: inode as _,
+			d_off: (off + len) as _,
+			d_reclen: len as _,
+			d_name: [],
+		};
+
+		// Write entry
+		unsafe {
+			ptr::write(&mut slice[off] as *mut _ as *mut _, ent);
+		}
+		// Copy file name
+		let name_slice = &mut slice[off + offset_of!(Self, d_name)..];
+		name_slice[..name.len()].copy_from_slice(name);
+		name_slice[name.len()] = 0;
+		// Write entry type
+		name_slice[name.len() + 1] = entry_type.to_dirent_type();
+	}
+}
+
+#[syscall]
+pub fn getdents(fd: c_uint, dirp: SyscallSlice<u8>, count: c_uint) -> Result<i32, Errno> {
+	do_getdents::<LinuxDirent>(fd, dirp, count as usize)
 }
--- src/syscall/getdents64.rs
+++ src/syscall/getdents64.rs
@@ -1,14 +1,12 @@
 //! The `getdents64` system call allows to get the list of entries in a given
 //! directory.
 
+use super::getdents::{do_getdents, Dirent};
 use crate::errno::Errno;
-use crate::file::FileContent;
+use crate::file::{FileType, INode};
 use crate::process::mem_space::ptr::SyscallSlice;
-use crate::process::Process;
 use core::ffi::c_int;
-use core::ffi::c_void;
-use core::mem::offset_of;
-use core::mem::size_of;
+use core::mem::{offset_of, size_of};
 use core::ptr;
 use macros::syscall;
 
@@ -23,95 +21,42 @@ struct LinuxDirent64 {
 	d_reclen: u16,
 	/// File type.
 	d_type: u8,
-	/// Filename (null-terminated).
+	/// Filename (nul-terminated).
 	d_name: [u8; 0],
 }
 
+impl Dirent for LinuxDirent64 {
+	fn required_length(name: &[u8]) -> usize {
+		(size_of::<Self>() + name.len() + 1)
+			// Padding for alignment
+			.next_multiple_of(size_of::<usize>())
+	}
+
+	fn write(slice: &mut [u8], off: usize, inode: INode, entry_type: FileType, name: &[u8]) {
+		let len = Self::required_length(name);
+		let ent = Self {
+			d_ino: inode,
+			d_off: (off + len) as _,
+			d_reclen: len as _,
+			d_type: entry_type.to_dirent_type(),
+			d_name: [],
+		};
+
+		// Write entry
+		unsafe {
+			ptr::write(&mut slice[off] as *mut _ as *mut _, ent);
+		}
+		// Copy file name
+		let name_slice = &mut slice[off + offset_of!(Self, d_name)..];
+		name_slice[..name.len()].copy_from_slice(name);
+		name_slice[name.len()] = 0;
+	}
+}
+
 #[syscall]
-pub fn getdents64(fd: c_int, dirp: SyscallSlice<c_void>, count: usize) -> Result<i32, Errno> {
+pub fn getdents64(fd: c_int, dirp: SyscallSlice<u8>, count: usize) -> Result<i32, Errno> {
 	if fd < 0 {
 		return Err(errno!(EBADF));
 	}
-
-	let (mem_space, open_file_mutex) = {
-		let proc_mutex = Process::current_assert();
-		let proc = proc_mutex.lock();
-
-		let mem_space = proc.get_mem_space().unwrap().clone();
-
-		let fds_mutex = proc.get_fds().unwrap().clone();
-		let fds = fds_mutex.lock();
-
-		let open_file_mutex = fds
-			.get_fd(fd as _)
-			.ok_or_else(|| errno!(EBADF))?
-			.get_open_file()
-			.clone();
-
-		(mem_space, open_file_mutex)
-	};
-
-	// Getting file
-	let mut open_file = open_file_mutex.lock();
-
-	let mut mem_space_guard = mem_space.lock();
-	let dirp_slice = dirp
-		.get_mut(&mut mem_space_guard, count)?
-		.ok_or_else(|| errno!(EFAULT))?;
-
-	let mut off = 0;
-	let mut entries_count = 0;
-	let start = open_file.get_offset();
-
-	{
-		let file_mutex = open_file.get_file();
-		let file = file_mutex.lock();
-
-		let entries = match file.get_content() {
-			FileContent::Directory(entries) => entries,
-			_ => return Err(errno!(ENOTDIR)),
-		};
-
-		// Iterating over entries and filling the buffer
-		for (name, entry) in entries.iter().skip(start as _) {
-			let len = size_of::<LinuxDirent64>() + name.len() + 1;
-			// If the buffer is not large enough, return an error
-			if off == 0 && len > count {
-				return Err(errno!(EINVAL));
-			}
-			// If reaching the end of the buffer, break
-			if off + len > count {
-				break;
-			}
-
-			let ent_ptr = &mut dirp_slice[off] as *mut _ as *mut LinuxDirent64;
-			let ent = LinuxDirent64 {
-				d_ino: entry.inode,
-				d_off: (off + len) as _,
-				d_reclen: len as _,
-				d_type: entry.entry_type.to_dirent_type(),
-				d_name: [],
-			};
-			let ent_name_ptr =
-				unsafe { (ent_ptr as *mut u8).add(offset_of!(LinuxDirent64, d_name)) };
-
-			// Copying file name
-			unsafe {
-				// Writing entry
-				ptr::write_unaligned(ent_ptr, ent);
-
-				// Copy file name
-				ptr::copy_nonoverlapping(name.as_bytes().as_ptr(), ent_name_ptr, name.len());
-
-				// Writing padding byte
-				*ent_name_ptr.add(name.len()) = 0;
-			}
-
-			off += len;
-			entries_count += 1;
-		}
-	}
-
-	open_file.set_offset(start + entries_count);
-	Ok(off as _)
+	do_getdents::<LinuxDirent64>(fd as _, dirp, count)
 }
