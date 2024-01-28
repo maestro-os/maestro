a5e36477d2ced9c6363a57f145595b7db57d162a
llenotre <llenotre@student.42.fr>
2024-01-28 23:31:33 +0100

    perf: strlen

--- src/util/libc/strlen.c
+++ src/util/libc/strlen.c
@@ -1,12 +1,22 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#define LOW ((size_t) -1 / 0xff)
+#define HIGH (LOW * 0x80)
+#define ZERO(w) (((w) - LOW) & (~(w) & HIGH))
+
 size_t strlen(const char *s)
 {
-	size_t n = 0;
+	const char *n = s;
 
-	// TODO optimize
-	while (s[n])
-		++n;
-	return n;
+    // Align
+    for (; (uintptr_t) n % sizeof(size_t); ++n) if (!*n) return n - s;
+    // Check word-by-word
+    const size_t *word = (size_t *) n;
+    for (; !ZERO(*word); ++word);
+    n = (const char *) word;
+    // Count remaining
+    for (; *n; ++n)
+        ;
+	return n - s;
 }
