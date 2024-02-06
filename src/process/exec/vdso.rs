569a8d3548420917e28b50839d8f7b75441c3e89
llenotre <llenotre@student.42.fr>
2024-02-07 00:36:01 +0100

    refactor: cleanup

--- src/process/exec/vdso.rs
+++ src/process/exec/vdso.rs
@@ -21,7 +21,7 @@
 
 use crate::{
 	elf::parser::ELFParser,
-	errno::Errno,
+	errno::{AllocResult, CollectResult, EResult},
 	include_bytes_aligned, memory,
 	memory::buddy,
 	process::{
@@ -46,7 +46,7 @@ struct Vdso {
 	entry_off: usize,
 }
 
-/// Informations about mapped vDSO.
+/// Information about mapped vDSO.
 pub struct MappedVDSO {
 	/// The virtual address to the beginning of the vDSO.
 	pub ptr: *mut c_void,
@@ -58,28 +58,32 @@ pub struct MappedVDSO {
 static VDSO: Mutex<Option<Vdso>> = Mutex::new(None);
 
 /// Loads the vDSO in memory and returns the image.
-fn load_image() -> Result<Vdso, Errno> {
+fn load_image() -> EResult<Vdso> {
 	let parser = ELFParser::new(ELF_IMAGE)?;
 	let entry_off = parser.hdr().e_entry as _;
-
 	// Load image into pages
 	let pages_count = ELF_IMAGE.len().div_ceil(memory::PAGE_SIZE);
-	// TODO collect
-	let mut pages = Vec::new();
-	for i in 0..pages_count {
-		let off = i * memory::PAGE_SIZE;
-		let len = min(memory::PAGE_SIZE, ELF_IMAGE.len() - off);
-		let ptr = unsafe {
-			// Alloc page
-			let mut ptr = buddy::alloc(0, buddy::FLAG_ZONE_TYPE_KERNEL)?;
-			let virt_ptr = memory::kern_to_virt(ptr.as_mut()) as _;
-			// Copy data
-			ptr::copy_nonoverlapping(ELF_IMAGE[off..].as_ptr() as *const c_void, virt_ptr, len);
-			ptr
-		};
-		pages.push(ptr.cast())?;
-	}
-
+	let pages = (0..pages_count)
+		.map(|i| {
+			let off = i * memory::PAGE_SIZE;
+			let len = min(memory::PAGE_SIZE, ELF_IMAGE.len() - off);
+			unsafe {
+				// Alloc page
+				// FIXME: free previously allocated pages on failure? In practice, this is not an
+				// issue as the system panics on failure
+				let mut ptr = buddy::alloc(0, buddy::FLAG_ZONE_TYPE_KERNEL)?;
+				let virt_ptr = memory::kern_to_virt(ptr.as_mut()) as _;
+				// Copy data
+				ptr::copy_nonoverlapping(
+					ELF_IMAGE[off..].as_ptr() as *const c_void,
+					virt_ptr,
+					len,
+				);
+				Ok(ptr.cast())
+			}
+		})
+		.collect::<AllocResult<CollectResult<_>>>()?
+		.0?;
 	Ok(Vdso {
 		pages: Arc::new(pages)?,
 		len: ELF_IMAGE.len(),
@@ -91,15 +95,9 @@ fn load_image() -> Result<Vdso, Errno> {
 /// Maps the vDSO into the given memory space.
 ///
 /// The function returns the virtual pointer to the mapped vDSO.
-pub fn map(mem_space: &mut MemSpace) -> Result<MappedVDSO, Errno> {
-	// TODO cleanup
+pub fn map(mem_space: &mut MemSpace) -> EResult<MappedVDSO> {
 	let mut elf_image = VDSO.lock();
-	if elf_image.is_none() {
-		let img = load_image().expect("Failed to load vDSO");
-		*elf_image = Some(img);
-	}
-	let img = elf_image.as_ref().unwrap();
-
+	let img = elf_image.get_or_insert_with(|| load_image().expect("Failed to load vDSO"));
 	let vdso_pages = img.len.div_ceil(memory::PAGE_SIZE);
 	let Some(vdso_pages) = NonZeroUsize::new(vdso_pages) else {
 		panic!("Invalid vDSO image");
@@ -113,9 +111,7 @@ pub fn map(mem_space: &mut MemSpace) -> Result<MappedVDSO, Errno> {
 			pages: img.pages.clone(),
 		},
 	)?;
-
 	let entry = NonNull::new(unsafe { ptr.add(img.entry_off) }).unwrap();
-
 	Ok(MappedVDSO {
 		ptr,
 		entry,
