9c8026575844c3aa0cd87d3e56a56cd2fbcd1a83
llenotre <llenotre@student.42.fr>
2024-02-07 00:36:01 +0100

    refactor: cleanup

--- src/memory/alloc.rs
+++ src/memory/alloc.rs
@@ -1,17 +1,16 @@
 //! This file handles memory allocators initialization for the kernel.
 //!
-//! The physical memory is divided into zones. Each zones contains frames that
+//! The physical memory is divided into zones. Each zones contain frames that
 //! can be allocated by the buddy allocator
 //!
 //! The following zones exist:
 //! - Kernel: Memory to be allocated by the kernel, shared across processes. This zone requires
-//! that every frames of virtual memory are associated with a unique physical
+//! that every frame of virtual memory are associated with a unique physical
 //! frame.
 //! - MMIO: Memory used for Memory Mapped I/O. This zones requires only virtual memory, thus it
 //! overlaps with the user zone which allocates the physical memory.
-//! - User: Memory used for userspace mappings. This zone doesn't requires virtual memory to
-//! correspond with the physical memory, thus it can be located outside of the
-//! kernelspace.
+//! - User: Memory used for userspace mappings. This zone doesn't require virtual memory to
+//! correspond with the physical memory, thus it can be located outside the kernelspace.
 
 use crate::{
 	memory,
