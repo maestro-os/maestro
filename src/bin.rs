c9e7cb1280186e0f628675f7c66f80870499373a
llenotre <llenotre@student.42.fr>
2024-02-07 19:11:23 +0100

    fix(clippy): tests check

--- src/bin.rs
+++ src/bin.rs
@@ -20,5 +20,8 @@
 
 #![no_std]
 #![no_main]
+// Force presence of the test code for both `cargo test` and `cargo clippy --tests`
+#![feature(custom_test_frameworks)]
+#![test_runner(kernel::selftest::runner)]
 
 extern crate kernel;
