45aee57cee37ce86e8bdd3615c46a1703b79ef5f
llenotre <llenotre@student.42.fr>
2024-08-24 11:17:55 +0200

    style: cargo fmt

--- utils/src/lock/atomic.rs
+++ utils/src/lock/atomic.rs
@@ -18,9 +18,7 @@
 
 //! Implementation of [`AtomicU64`].
 
-use core::fmt;
-use core::fmt::Formatter;
-use core::sync::atomic;
+use core::{fmt, fmt::Formatter, sync::atomic};
 
 /// Fulfills the role of `AtomicU64`, while being available on 32 bits platforms.
 #[cfg(target_has_atomic = "64")]
@@ -33,63 +31,63 @@ pub struct AtomicU64(core::sync::atomic::AtomicU64);
 pub struct AtomicU64(super::IntMutex<u64>);
 
 impl AtomicU64 {
-    /// Creates a new instance with the given value.
-    pub const fn new(val: u64) -> Self {
-        #[cfg(target_has_atomic = "64")]
-        {
-            Self(core::sync::atomic::AtomicU64::new(val))
-        }
-        #[cfg(not(target_has_atomic = "64"))]
-        {
-            Self(super::IntMutex::new(val))
-        }
-    }
+	/// Creates a new instance with the given value.
+	pub const fn new(val: u64) -> Self {
+		#[cfg(target_has_atomic = "64")]
+		{
+			Self(core::sync::atomic::AtomicU64::new(val))
+		}
+		#[cfg(not(target_has_atomic = "64"))]
+		{
+			Self(super::IntMutex::new(val))
+		}
+	}
 
-    /// Loads a value from the atomic integer.
-    #[allow(unused_variables)]
-    pub fn load(&self, order: atomic::Ordering) -> u64 {
-        #[cfg(target_has_atomic = "64")]
-        {
-            self.0.load(order)
-        }
-        #[cfg(not(target_has_atomic = "64"))]
-        {
-            *self.0.lock()
-        }
-    }
+	/// Loads a value from the atomic integer.
+	#[allow(unused_variables)]
+	pub fn load(&self, order: atomic::Ordering) -> u64 {
+		#[cfg(target_has_atomic = "64")]
+		{
+			self.0.load(order)
+		}
+		#[cfg(not(target_has_atomic = "64"))]
+		{
+			*self.0.lock()
+		}
+	}
 
-    /// Stores a value into the atomic integer.
-    #[allow(unused_variables)]
-    pub fn store(&self, val: u64, order: atomic::Ordering) {
-        #[cfg(target_has_atomic = "64")]
-        {
-            self.0.store(val, order)
-        }
-        #[cfg(not(target_has_atomic = "64"))]
-        {
-            *self.0.lock() = val;
-        }
-    }
+	/// Stores a value into the atomic integer.
+	#[allow(unused_variables)]
+	pub fn store(&self, val: u64, order: atomic::Ordering) {
+		#[cfg(target_has_atomic = "64")]
+		{
+			self.0.store(val, order)
+		}
+		#[cfg(not(target_has_atomic = "64"))]
+		{
+			*self.0.lock() = val;
+		}
+	}
 
-    /// Adds to the current value, returning the previous value.
-    #[allow(unused_variables)]
-    pub fn fetch_add(&self, val: u64, order: atomic::Ordering) -> u64 {
-        #[cfg(target_has_atomic = "64")]
-        {
-            self.0.fetch_add(val, order)
-        }
-        #[cfg(not(target_has_atomic = "64"))]
-        {
-            let mut guard = self.0.lock();
-            let prev = *guard;
-            *guard = guard.wrapping_add(val);
-            prev
-        }
-    }
+	/// Adds to the current value, returning the previous value.
+	#[allow(unused_variables)]
+	pub fn fetch_add(&self, val: u64, order: atomic::Ordering) -> u64 {
+		#[cfg(target_has_atomic = "64")]
+		{
+			self.0.fetch_add(val, order)
+		}
+		#[cfg(not(target_has_atomic = "64"))]
+		{
+			let mut guard = self.0.lock();
+			let prev = *guard;
+			*guard = guard.wrapping_add(val);
+			prev
+		}
+	}
 }
 
 impl fmt::Debug for AtomicU64 {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        fmt::Debug::fmt(&self.0, f)
-    }
+	fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+		fmt::Debug::fmt(&self.0, f)
+	}
 }
