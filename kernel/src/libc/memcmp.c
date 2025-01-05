b5a3d064b213e97505756cd3ea942bcd4d2eab27
llenotre <llenotre@student.42.fr>
2025-01-05 22:10:53 +0100

    fix: `memcmp`

--- kernel/src/libc/memcmp.c
+++ kernel/src/libc/memcmp.c
@@ -1,59 +1,10 @@
-/*
- * Copyright 2024 Luc Lenôtre
- *
- * This file is part of Maestro.
- *
- * Maestro is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version.
- *
- * Maestro is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * Maestro. If not, see <https://www.gnu.org/licenses/>.
- */
+// Code taken from musl. License: https://git.musl-libc.org/cgit/musl/tree/COPYRIGHT
 
-#include <stdint.h>
 #include <stddef.h>
 
-// Gives the offset of the pointer `ptr` relative to its down-aligned
-// counterpart.
-# define ALIGN_MASK(ptr, n)	((intptr_t) (ptr) & ((n) - 1))
-
-// Tells whether the pointer `ptr` is aligned on boundary `n`.
-//
-// If `n` is zero, the behaviour is undefined.
-# define IS_ALIGNED(ptr, n)	(ALIGN_MASK(ptr, n) == 0)
-
-// Aligns down the given memory pointer `ptr` to the boundary `n`.
-//
-// If `n` is zero, the behaviour is undefined.
-# define DOWN_ALIGN(ptr, n)\
-	(typeof(ptr)) ((intptr_t) (ptr) & ~((intptr_t) ((n) - 1)))
-
-int memcmp(const void *s1, const void *s2, size_t n)
+int memcmp(const void *vl, const void *vr, size_t n)
 {
-	// The index of the current byte
-	size_t i;
-	// The end of the aligned portion of memory
-	size_t align_end;
-
-	i = 0;
-	align_end = (size_t) (DOWN_ALIGN(s1 + n, sizeof(long)) - s1);
-	while (i < n
-		&& !(IS_ALIGNED(s1, sizeof(long)) && IS_ALIGNED(s2, sizeof(long)))
-		&& ((volatile char *) s1)[i] == ((volatile char *) s2)[i])
-		++i;
-	while (i < align_end
-		&& *((volatile long *) (s1 + i)) == *((volatile long *) (s2 + i)))
-		++i;
-	while (i < n
-		&& ((volatile char *) s1)[i] == ((volatile char *) s2)[i])
-		++i;
-	if (i >= n)
-		return 0;
-	return (((unsigned char *) s1)[i] - ((unsigned char *) s2)[i]);
+    const unsigned char *l = vl, *r = vr;
+    for (; n && *l == *r; n--, l++, r++);
+    return n ? *l - *r : 0;
 }
