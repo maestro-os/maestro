273043fb0c2990868a5e7d1e87d4ca77fddc8085
llenotre <llenotre@student.42.fr>
2025-04-14 15:10:26 +0200

    ci: log message

--- kernel/ci/test.sh
+++ kernel/ci/test.sh
@@ -12,18 +12,14 @@ case $1 in
 		cargo test --lib $CARGOFLAGS
 		;;
 	int)
-		set +e
 		cargo run $CARGOFLAGS
-		STATUS=$?
-		set -e
 
 		# Check filesystem integrity
 		# FIXME: the clock currently starts at the timestamp zero, which causes fsck to detect errors due to the low value for dtime
 		#fsck.ext2 -fnv qemu_disk
 		# Check persistence
-		echo 'cat /persistent' | debugfs -f - qemu_disk 2>&1 | grep 'persistence OK' 2>&1 >/dev/null
-
-		exit $STATUS
+		echo 'Check `/persistent` exists'
+		echo 'cat /persistent' | debugfs -f - qemu_disk 2>&1 | grep 'persistence OK'
 		;;
 	*)
 		>&2 echo "Invalid tests kind"
