6209f39101838b3bf9392e5ab901afb1e45b5eea
llenotre <llenotre@student.42.fr>
2025-11-28 01:52:08 +0100

    fix: `rename` (wip)

--- kernel/src/file/fs/tmp/mod.rs
+++ kernel/src/file/fs/tmp/mod.rs
@@ -291,7 +291,9 @@ impl NodeOps for DirectoryContent {
 		}
 		// If the source and destination are the same node, do nothing
 		let node = entry.node();
-		if let Some(new_ent) = new_ent && new_ent.node.inode == node.inode {
+		if let Some(new_ent) = new_ent
+			&& new_ent.node.inode == node.inode
+		{
 			return Ok(());
 		}
 		let node_ops = NodeContent::from_ops(&*node.node_ops);
