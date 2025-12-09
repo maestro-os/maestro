5e168870ae5a28d120ff9656d0651fef200d436e
llenotre <llenotre@student.42.fr>
2025-12-09 22:48:19 +0100

    feat(vfs): `CachePolicy`

--- kernel/src/file/fs/mod.rs
+++ kernel/src/file/fs/mod.rs
@@ -36,7 +36,7 @@ use crate::{
 	device::BlkDev,
 	file::{
 		perm::{AccessProfile, S_ISGID},
-		vfs::node::Node,
+		vfs::{CachePolicy, node::Node},
 	},
 	memory::{
 		PhysAddr,
@@ -422,8 +422,8 @@ impl FileOps for DummyOps {}
 pub trait FilesystemOps: Any + Debug {
 	/// Returns the name of the filesystem.
 	fn get_name(&self) -> &[u8];
-	/// Tells whether the directory of this filesystem can be cached.
-	fn cache_entries(&self) -> bool;
+	/// Returns the filesystem's cache policy
+	fn cache_policy(&self) -> CachePolicy;
 
 	/// Returns statistics about the filesystem.
 	fn get_stat(&self) -> EResult<Statfs>;
